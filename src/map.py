ea1dfc3c093d94bd6a635af00d1a7e4154a95189
Peter Huson <peter@huson.com>
2019-01-29 11:21:04 -0500

    Switching to an adjacency matrix

--- src/map.py
+++ src/map.py
@@ -1,19 +1,21 @@
 import numpy as np
+from scipy.sparse.csgraph import dijkstra
 
 class Maze:
 	def __init__(self, width=16, height=16):
 		self.width = width
 		self.height = height
 
-		# store the north-south walls in v_walls and east-west walls in h_walls
-		self.h_walls = np.zeros([self.height + 1, self.width])
-		self.v_walls = np.zeros([self.height, self.width + 1])
+		self.adj_matrix = np.ones([self.width * self.height, self.width * self.height])
+		self.disconnect_non_adjacent()
 
-		self.add_perimeter_walls()
-
-	def add_perimeter_walls(self):
-		self.v_walls[:, [0,-1]] = 1
-		self.h_walls[[0,-1], :] = 1
+	def disconnect_non_adjacent(self):
+		for c1_index in xrange(self.width * self.height):
+			for c2_index in xrange(self.width * self.height):
+				c1 = self.find_x_y(c1_index)
+				c2 = self.find_x_y(c2_index)
+				self.adj_matrix[c1, c2] = ((abs(c1[0] - c2[0]) + abs(c1[1] - c2[1])) == 1)
+		
 
 	def get_connected(self, c1, c2):
 		"""Check whether a wall exists between adjacent cells
@@ -26,10 +28,10 @@ class Maze:
 		    float: probability of wall between those two cells
 		"""
 		assert (abs(c1[0] - c2[0]) + abs(c1[1] - c2[1])) == 1, 'Cells must be adjacent.'
-		if c1[0] == c2[0]:
-			return self.h_walls[max(c1[1], c2[1]), c1[0]]
-		else:
-			return self.v_walls[c1[1], max(c1[0], c2[0])]
+		c1_cell_number = self.find_adjacency_cell_number(c1)
+		c2_cell_number = self.find_adjacency_cell_number(c2)
+
+		return self.adj_matrix[max(c1_cell_number,c2_cell_number),min(c1_cell_number, c2_cell_number)]
 
 	def set_connected(self, c1, c2, v):
 		"""Set whether a wall exists between adjacent cells
@@ -40,29 +42,59 @@ class Maze:
 		    v  (float): probability of a wall between the cells
 		"""
 		assert (abs(c1[0] - c2[0]) + abs(c1[1] - c2[1])) == 1, 'Cells must be adjacent.'
-		if c1[0] == c2[0]:
-			self.h_walls[max(c1[1], c2[1]), c1[0]] = v
-		else:
-			self.v_walls[c1[1], max(c1[0], c2[0])] = v
+		c1_cell_number = self.find_adjacency_cell_number(c1)
+		c2_cell_number = self.find_adjacency_cell_number(c2)
 
-	def __str__(self):
-		output = ''
-		for row in range(self.height + 1):
-			horizontal = ['+---' if w > 0 else '+   ' for w in self.h_walls[row,:]]
-			output += (''.join(horizontal) + '+\n')
+		self.adj_matrix[max(c1_cell_number,c2_cell_number),min(c1_cell_number, c2_cell_number)] = v
 
-			if row < self.height:
-				vertical = ['|   ' if w > 0 else '    ' for w in self.v_walls[row,:]]
-				output += (''.join(vertical)[:-3] + '\n')
+	# TODO: Fix me! 
+	# def __str__(self):
+	# 	output = ''
+	# 	for row in range(self.height + 1):
+	# 		horizontal = ['+---' if w > 0 else '+   ' for w in self.h_walls[row,:]]
+	# 		output += (''.join(horizontal) + '+\n')
 
-		return output
+	# 		if row < self.height:
+	# 			vertical = ['|   ' if w > 0 else '    ' for w in self.v_walls[row,:]]
+	# 			output += (''.join(vertical)[:-3] + '\n')
+
+		# return output
+
+	def find_route(self, c1, c2, threshold = 0):
+		r = dijkstra(self.adj_matrix, directed = False, unweighted = True, return_predecessors = True)
+		c1_index = self.find_adjacency_cell_number(c1)
+		c2_index = self.find_adjacency_cell_number(c2)
+		path = []
+		print(r[1])
+		self.unwind_predecessor_matrix(r[1], c1_index, c2_index, path)
+		return path
+
+	def unwind_predecessor_matrix(self, r, c1, c2, path):
+		intermediate_cell = r[c1, c2]
+		if intermediate_cell < 0:
+			return 
+		path.append(intermediate_cell)
+		print("intermediate: %s c1: %s c2: %s path: %s" % (intermediate_cell, c1, c2, path))
+		self.unwind_predecessor_matrix(r, c1, intermediate_cell, path)
+
+	def find_adjacency_cell_number(self, c1):
+		return self.width * c1[1] + c1[0]
+
+	def find_x_y(self, n):
+		return np.array([n % self.width, np.floor(n / self.width)], dtype=int)
+
+	def build_adjacency(self):
+		self.v_walls
+		self.h_walls
 
 
 if __name__ == '__main__':
-	m = Maze(5,4)
+	m = Maze(2,2)
 	# add some walls
-	m.h_walls[1,1] = 1
-	m.h_walls[2,3] = 1
-	m.v_walls[2,3] = 1
+	# m.h_walls[1,1] = 1
+	# m.h_walls[2,3] = 1
+	# m.v_walls[2,3] = 1
+	# m.set_connected([1,1],[0,1], 1)
+	# m.set_connected([1,2],[1,1], 1)
 	# see what it looks like
 	print(m)
