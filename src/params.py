543da3d204fd642b878dab01d4b1fc2f090b8dce
Izzy Brand <isaiah_brand@brown.edu>
2019-02-07 09:22:43 -0500

    changed params to meters

--- src/params.py
+++ src/params.py
@@ -1,9 +1,9 @@
 # MAZE
-maze_thickness = 1.2 # cm
-maze_tolerance = 0.05 # 
-maze_inner_size = 16.8 # cm (not including the thickness of the walls)
+maze_thickness = 0.012 # m
+maze_tolerance = 0.0005 # m
+maze_inner_size = 0.168 # m (not including the thickness of the walls)
 
 # ROBOT
 lidar_transforms = [] # TODO: a list of lidar positions relative to the robot origin
-robot_length = 8 # TODO: these are random numbers
-robot_width = 6
+robot_length = 0.08 # TODO: these are random numbers
+robot_width = 0.06
