e8be954128f0ab01579fe41faf213e21274a0dd6
Izzy Brand <isaiah_brand@brown.edu>
2019-02-07 11:47:40 -0500

    added function to generate maze as line segments and visualize with matplotlib

--- src/estimation.py
+++ src/estimation.py
@@ -1,24 +1,25 @@
 from map import Maze
 import params as p
+import numpy as np
 
-def get_2d_rotation(theta):
-    return np.array([[np.cos(theta), -np.sin(theta)],
-                     [np.sin(theta), np.cos(theta)]])
+# def get_2d_rotation(theta):
+#     return np.array([[np.cos(theta), -np.sin(theta)],
+#                      [np.sin(theta), np.cos(theta)]])
 
-class Pose:
-    def __init__(self, t=np.zeros(2), r=np.zeros(2,2)):
-        assert t.size == r.shape[0] == r.shape[1]
-        self.t = t
-        self.rotation = r
-        self.dim = self.t.size
+# class Pose:
+#     def __init__(self, t=np.zeros(2), r=np.zeros(2,2)):
+#         assert t.size == r.shape[0] == r.shape[1]
+#         self.t = t
+#         self.rotation = r
+#         self.dim = self.t.size
 
-    def __mul__(self, other):
-        # TODO: apply to another pose
-        pass
+#     def __mul__(self, other):
+#         # TODO: apply to another pose
+#         pass
 
-    def inverse(self):
-        # TODO: get the inverse
-        pass
+#     def inverse(self):
+#         # TODO: get the inverse
+#         pass
 
 # TODO:
 # * settle on a representation for the robot pose -- rotation, translation (2D?) 
@@ -26,10 +27,59 @@ class Pose:
 # * write a func to get estiamted lidar returns (distributions given a pose and a maze)
 # * 
 
+def rotate2d(coord, theta):
+    r = np.array([[np.cos(theta), -np.sin(theta)],
+                  [np.sin(theta),  np.cos(theta)]])
+    return np.dot(r, coord)
 
 def estimate_lidar_returns(pose, maze):
+    segment_list = maze_to_segment_list(maze)
     for lidar_transform in p.lidar_transforms:
-        pass
+        lidar_global_xy = pose[:2] + rotate2d(lidar_transform[:2], pose[2])
+        lidar_global_theta = pose[2] + lidar_transform[2]
+        lidar_global_end = rotate2d([5.,0], lidar_global_theta)
+        segment_list.append(lidar_global_xy.tolist() + lidar_global_end.tolist())
 
-if __name__ = '__main__':
-    pass
+    from matplotlib import pyplot as plt
+
+    for seg in segment_list[:-6]:
+        plt.plot((seg[0], seg[2]), (seg[1], seg[3]), 'k')
+    for seg in segment_list[-6:]:
+        plt.plot((seg[0], seg[2]), (seg[1], seg[3]), 'r')
+    plt.show()
+
+
+def maze_to_segment_list(maze):
+    segment_list = []
+    c = p.maze_inner_size
+    for x in range(maze.width):
+        for y in range(maze.height):
+            if x < maze.width - 1:
+                if not maze.get_connected([x,y], [x+1,y]):
+                    segment_list.append([(x+1)*c, y*c, (x+1)*c, (y+1)*c])
+            if y < maze.height - 1:
+                if not maze.get_connected([x,y], [x,y+1]):
+                    segment_list.append([x*c, (y+1)*c, (x+1)*c, (y+1)*c])
+
+
+    segment_list.append([0, 0, maze.width*c, 0])
+    segment_list.append([0, 0, 0, maze.height*c])
+    segment_list.append([maze.width*c,0, maze.width*c, maze.height*c])
+    segment_list.append([0, maze.height*c, maze.width*c, maze.height*c])
+    return segment_list
+
+def test_generate_segment_list():
+    # verify that generate segment list works as intended
+    from matplotlib import pyplot as plt
+    m = Maze(6,5)
+    print m
+    segment_list = maze_to_segment_list(m)
+    for seg in segment_list:
+        plt.plot((seg[0], seg[2]), (seg[1], seg[3]), 'k')
+    plt.show()
+
+if __name__ == '__main__':
+    # test_generate_segment_list()
+    pose = [0.1,0.2, np.pi/4]
+    m = Maze(16,16)
+    estimate_lidar_returns(pose, m)
--- src/params.py
+++ src/params.py
@@ -6,11 +6,11 @@ maze_tolerance = 0.0005 # m
 maze_inner_size = 0.168 # m (not including the thickness of the walls)
 
 # ROBOT
-lidar_transforms = np.array([[0.047535,	-0.038236,	-np.pi/2], # x, y, theta (body frame)
+lidar_transforms = np.array([[0.047535,	-0.038236,	-np.pi/2.0], # x, y, theta (body frame)
 							 [0.051362,	-0.024628,	0.0],
-							 [0.059890,	-0.009768,	-np.pi/4],
-							 [0.059890,	0.009768,	np.pi/4],
+							 [0.059890,	-0.009768,	-np.pi/4.0],
+							 [0.059890,	0.009768,	np.pi/4.0],
 							 [0.051362,	0.024628,	0.0],
-							 [0.047535,	0.038236,	np.pi/2]])
+							 [0.047535,	0.038236,	np.pi/2.0]])
 robot_length = 0.08 # TODO: these are random numbers
 robot_width = 0.06
