5e4038cffe65146fc984cc38c8d144837d40d193
Izzy Brand <isaiah_brand@brown.edu>
2019-02-15 14:26:35 -0500

    condensed particle filter and switched particle array axes

--- src/ParticleFilterExample/ParticleFilter.py
+++ src/ParticleFilterExample/ParticleFilter.py
@@ -5,68 +5,32 @@ def particle_filter_update(X, u_mu, u_sigma, Z, obs_func):
     """ Update step for a particle filter
 
         Args:
-            X (r x c numpy array): Each column is a particle, each row corresponds to a state var
+            X (c x r numpy array): Each row is a particle, each column corresponds to a state var
             u_mu (r x 1 numpy array): Noise mean for motion model
-            u_sigma (r x 1 numpy array): Noise stdev 
+            u_sigma (r x 1 numpy array): Noise stdev
             Z (s x 1 numpy array): Sensor returns for each sensor
             obs_func ( (s x 1 array), (r x 1 array) -> float): P(Z | x)
 
         Returns:
             (r x c numpy_array): New particles
     """
-    
-    n_particles = X.shape[1]
-    n_dimensions = X.shape[0]
-
     # Sample the motion model of existing particles
-    X_perturbed = X + np.random.normal(u_mu, u_sigma, size=(n_particles,n_dimensions)).transpose()
-
+    X_perturbed = X + np.random.normal(u_mu, u_sigma, size=X.shape)
     # find P(Z | x) for each perturbed particle
-    likelihoods = np.zeros(n_particles)
-    for ix in range(n_particles):
-        likelihoods[ix] = obs_func(Z, X_perturbed[:,ix])
+    likelihoods = np.array([obs_func(Z, x) for x in X_perturbed])
+    # resample the particles with replacement
+    return resample(X_perturbed, likelihoods/np.sum(likelihoods))
 
-    # The most likely sample will be resampled at most this much more frequently than the least
-    # likely
-    max_resample_weight = 500
+def resample(X, pmf):
+    """ Given c particles and a c-element pmf, choose c new particles from the input particles
+    with replacement, according to the specified pmf
 
-    X_bel_prob = np.zeros(n_particles)
-    # Resample the particles weighted by P(Z | x)
-    for m in range(n_particles):
-        if m == 1:
-            ix = 1
-        else:
-            ix = m-1
+    Args:
+        X (c x r numpy array): Each row is a particle, each column corresponds to a state var
+        pmf (c x 1 numpy array): the likelihood of each particle given the sensor data
 
-        X_bel_prob[m] = X_bel_prob[ix] + max_resample_weight*likelihoods[m]
-        # each "segment" of this has length proportional to the weight
-
-    X_new = resample(X_perturbed, X_bel_prob)
-
-    return X_new
-
-def resample(X, prob):
-    """ Resamples the Particles in X based on the probability vector
-
-        Args:
-            X (r x c numpy array): Samples from the columns of X (with replacement) based on the 
-                probabilities in prob
-            prob ([float]): An increasing list of numbers, where (prob[i+1]-prob[i])/prob[-1]
-                represents the probability of of x_i
-
-            Returns:
-                r x c numpy array: Resampled values of X
+    Returns:
+        TYPE: Description
     """
-
-    (_,n_samples) = X.shape
-    sample_indices = sorted(np.random.uniform(low=0, high=max(prob), size=n_samples))
-
-    X_resamp = np.zeros(X.shape)
-    nx = 0
-    for ix in range(len(prob)):
-        cprob = prob[ix]
-        while (nx < len(sample_indices)) and (cprob > sample_indices[nx]):
-            X_resamp[:,nx] = X[:,ix]
-            nx += 1
-
-    return X_resamp
+    new_ix = np.random.choice(len(pmf), size=X.shape[0], replace=True, p=pmf)
+    return X[new_ix, :]
--- src/ParticleFilterExample/ParticleFilterTests.py
+++ src/ParticleFilterExample/ParticleFilterTests.py
@@ -5,7 +5,7 @@ import matplotlib.animation as animation
 from ParticleFilter import particle_filter_update
 
 class ParticleFilterTests(object):
-    
+
     def __init__(self):
         self.mu = np.array([0,0])
         self.sigma = np.array([[5,0],[0,5]])
@@ -17,13 +17,13 @@ class ParticleFilterTests(object):
                 x = xpoints[ix]
                 y = ypoints[iy]
                 self.zpoints[iy][ix] = self.gaussian_2d(self.mu, self.sigma, x, y)
-                
+
         self.n_parts = 1000
         self.real_obj = np.array([-4,-4])
-        self.particles = np.random.uniform(low=-5,high=5, size=(2, self.n_parts))
+        self.particles = np.random.uniform(low=-5,high=5, size=(self.n_parts, 2))
         self.u_mu = np.array([.1,.1])
         self.u_sigma = np.array([.05,.05])
-    
+
     def gaussian_2d(self, mu, sigma, x, y):
         X = np.array([x,y])
         return np.exp(-1/2 * (X - mu).transpose() @ np.linalg.inv(sigma) @ (X - mu))/(np.sqrt((2*np.pi)**2 * np.linalg.det(sigma)))
@@ -37,7 +37,7 @@ fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
 
 def animate_plot(i, pf):
-    
+
     pf.real_obj = pf.real_obj + np.random.normal(pf.u_mu, pf.u_sigma)
     #Z = gaussian_2d(pf.mu, pf.sigma, pf.real_obj[0], pf.real_obj[1]) + np.random.normal(0, .01)
     Z = pf.gaussian_2d(pf.mu, pf.sigma, pf.real_obj[0], pf.real_obj[1])
@@ -45,11 +45,11 @@ def animate_plot(i, pf):
     pf.particles = particle_filter_update(pf.particles, pf.u_mu, pf.u_sigma, Z, pf.obs_func)
     ax1.clear()
     ax1.imshow(pf.zpoints,extent=[-5,5,-5,5])
-    ax1.scatter(pf.particles[0,:], pf.particles[1,:], color='r', alpha=.2)
+    ax1.scatter(pf.particles[:,0], pf.particles[:,1], color='r', alpha=.2)
     ax1.scatter(pf.real_obj[0], pf.real_obj[1], color='b')
 
 if __name__ == "__main__":
     pf = ParticleFilterTests()
-    num_iterations = 10
+    num_iterations = 80
     animation = animation.FuncAnimation(fig, animate_plot, frames=num_iterations, repeat=False, fargs=(pf,), interval=10)
     plt.show()
