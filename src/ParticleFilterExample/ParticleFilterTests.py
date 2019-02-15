2368660d0c63fac56b6324f8d06cabe68d1b77d3
Izzy Brand <isaiah_brand@brown.edu>
2019-02-15 15:07:30 -0500

    moved animation into class and python3 dependence

--- src/ParticleFilterExample/ParticleFilterTests.py
+++ src/ParticleFilterExample/ParticleFilterTests.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from ParticleFilter import particle_filter_update
 
-class ParticleFilterTests(object):
+class SimpleParticleFilterTest(object):
 
     def __init__(self):
         self.mu = np.array([0,0])
@@ -26,30 +26,31 @@ class ParticleFilterTests(object):
 
     def gaussian_2d(self, mu, sigma, x, y):
         X = np.array([x,y])
-        return np.exp(-1/2 * (X - mu).transpose() @ np.linalg.inv(sigma) @ (X - mu))/(np.sqrt((2*np.pi)**2 * np.linalg.det(sigma)))
+        return np.exp(-1/2 * np.dot((X - mu).transpose(),np.dot(np.linalg.inv(sigma),(X - mu))))/(np.sqrt((2*np.pi)**2 * np.linalg.det(sigma)))
 
     def obs_func(self, Z, x):
         """ P(Z | x) """
         z_exp = self.gaussian_2d(self.mu, self.sigma, x[0], x[1])
         return np.exp(-(100*(z_exp - Z))**2)
 
-fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
 
-def animate_plot(i, pf):
+    def animate_plot(self, i):
 
-    pf.real_obj = pf.real_obj + np.random.normal(pf.u_mu, pf.u_sigma)
-    #Z = gaussian_2d(pf.mu, pf.sigma, pf.real_obj[0], pf.real_obj[1]) + np.random.normal(0, .01)
-    Z = pf.gaussian_2d(pf.mu, pf.sigma, pf.real_obj[0], pf.real_obj[1])
+        self.real_obj = self.real_obj + np.random.normal(self.u_mu, self.u_sigma)
+        #Z = gaussian_2d(self.mu, self.sigma, self.real_obj[0], self.real_obj[1]) + np.random.normal(0, .01)
+        Z = self.gaussian_2d(self.mu, self.sigma, self.real_obj[0], self.real_obj[1])
 
-    pf.particles = particle_filter_update(pf.particles, pf.u_mu, pf.u_sigma, Z, pf.obs_func)
-    ax1.clear()
-    ax1.imshow(pf.zpoints,extent=[-5,5,-5,5])
-    ax1.scatter(pf.particles[:,0], pf.particles[:,1], color='r', alpha=.2)
-    ax1.scatter(pf.real_obj[0], pf.real_obj[1], color='b')
+        self.particles = particle_filter_update(self.particles, self.u_mu, self.u_sigma, Z, self.obs_func)
+        ax1.clear()
+        ax1.imshow(self.zpoints,extent=[-5,5,-5,5])
+        ax1.scatter(self.particles[:,0], self.particles[:,1], color='r', alpha=.2)
+        ax1.scatter(self.real_obj[0], self.real_obj[1], color='b')
+
 
 if __name__ == "__main__":
-    pf = ParticleFilterTests()
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1,1,1)
+    pf = SimpleParticleFilterTest()
     num_iterations = 80
-    animation = animation.FuncAnimation(fig, animate_plot, frames=num_iterations, repeat=False, fargs=(pf,), interval=10)
+    animation = animation.FuncAnimation(fig, pf.animate_plot, frames=num_iterations, repeat=False, interval=10)
     plt.show()
