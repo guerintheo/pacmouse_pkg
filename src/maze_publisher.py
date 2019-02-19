8cd751ad302399f670983368a41080f4019b240e
Theo Guerin <theocguerin@gmail.com>
2019-02-19 15:00:54 -0500

    Changed map to maze

--- src/maze_publisher.py
+++ src/maze_publisher.py
@@ -1,16 +1,16 @@
 import rospy
-from map import Maze
-from pacmouse_pkg.msg import Map
+from maze import Maze
+from pacmouse_pkg.msg import Maze
 
 
-class MapPublisher:
+class MazePublisher:
     """
     Class that publishes the adjacency matrix representing the maze.
     """
     
     def __init__(self):
-        rospy.init_node('map_publisher')
-        self.map_pub = rospy.Publisher('/pacmouse/map', Map, queue_size=1)
+        rospy.init_node('maze_publisher')
+        self.maze_pub = rospy.Publisher('/pacmouse/maze', Maze, queue_size=1)
         self.maze_width = 16
         self.maze_height= 16
         self.maze = Maze(self.maze_width, self.maze_height)
@@ -19,11 +19,11 @@ class MapPublisher:
     def publish_loop(self):
         rate = rospy.Rate(30)  # 30 Hz
         while not rospy.is_shutdown():
-            m = Map()
+            m = Maze()
             m.adj_matrix = self.maze.adj_matrix.flatten()
-            self.map_pub.publish(m)
+            self.maze_pub.publish(m)
             rate.sleep()
     
 if __name__ == '__main__':
-    map_publisher = MapPublisher()
-    map_publisher.publish_loop()
+    maze_publisher = MazePublisher()
+    maze_publisher.publish_loop()
