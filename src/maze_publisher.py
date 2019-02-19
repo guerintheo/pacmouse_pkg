da4617d49c1b1233d8c8e121e3abc15bf38080c8
Theo Guerin <theocguerin@gmail.com>
2019-02-19 15:14:05 -0500

    Modified maze_publisher

--- src/maze_publisher.py
+++ src/maze_publisher.py
@@ -1,6 +1,6 @@
 import rospy
 from maze import Maze
-from pacmouse_pkg.msg import Maze
+from pacmouse_pkg.msg import Maze as MazeMsg
 
 
 class MazePublisher:
@@ -10,16 +10,16 @@ class MazePublisher:
     
     def __init__(self):
         rospy.init_node('maze_publisher')
-        self.maze_pub = rospy.Publisher('/pacmouse/maze', Maze, queue_size=1)
+        self.maze_pub = rospy.Publisher('/pacmouse/maze', MazeMsg, queue_size=1)
         self.maze_width = 16
         self.maze_height= 16
-        self.maze = Maze(self.maze_width, self.maze_height)
+        self.maze = Maze(width=self.maze_width, height=self.maze_height)
         print(self.maze)
         
     def publish_loop(self):
         rate = rospy.Rate(30)  # 30 Hz
         while not rospy.is_shutdown():
-            m = Maze()
+            m = MazeMsg()
             m.adj_matrix = self.maze.adj_matrix.flatten()
             self.maze_pub.publish(m)
             rate.sleep()
