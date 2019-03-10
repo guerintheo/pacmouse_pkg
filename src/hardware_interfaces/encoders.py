4c29fbe2491c82bb5c001b148951570c205368c2
Izzy Brand <isaiah_brand@brown.edu>
2019-03-10 18:07:11 -0400

    encoders class now uses hardware interupts to estimate speeds

--- src/hardware_interfaces/encoders.py
+++ src/hardware_interfaces/encoders.py
@@ -3,108 +3,54 @@ import time
 import pacmouse_pkg.src.params as p
 import numpy as np
 from multiprocessing import Process, Array
+from collections import deque
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
 class Encoders:
     def __init__(self):
-        for pin in p.encoder_pins:
+        self.prevs = dict()
+        
+        for i, pin in enumerate(p.encoder_pins):
             GPIO.setup(pin, GPIO.IN)
-        self.sample_duration = 0.1
+            GPIO.add_event_detect(pin, GPIO.BOTH, lambda p: self._callback(p))
+            self.prevs[pin] = []
+
+    def _callback(self, pin):
+        # rising = GPIO.input(pin)
+        self.prevs[pin].append(time.time())
 
 
-    def get_speed(self):
-        start = time.time()
-        counts = np.zeros(4)
-        times = np.zeros(4)
-        corresponding = [1, 0, 3, 2]
-        prev_times = np.ones(4) * time.time()
-        prevs = np.array([GPIO.input(pin) for pin in p.encoder_pins])
+    def speed(self, history=0.1):
+        # TODO(izzy): estimate the direction of rotation by looking at the time diffs
+        # between the a and b pins for each wheel
+        
+        t = time.time()
+        counts = dict()
 
-        while time.time() - start < self.sample_duration:
-            t = time.time()
-            # get the newest encoder value
-            vals = np.array([GPIO.input(pin) for pin in p.encoder_pins])
+        for k in self.prevs.keys():
+            prev = np.array(self.prevs[k])
+            prev = prev[prev > t-history]
+            counts[k] = prev.size
+            self.prevs[k] = list(prev)
 
-            # see which pins have changed state
-            changed = (vals != prevs)
+        return ((counts[p.enc_l_a] + counts[p.enc_l_b])/(2.*history*p.gear_ratio),
+                (counts[p.enc_r_a] + counts[p.enc_r_b])/(2.*history*p.gear_ratio))
 
-            # # update the most recent flip times for the encoders that have chagned
-            # prev_times[changed] = t
-
-            # times[corresponding[changed]] += t - prevtimes[changed]
-
-            # # increment the counts for the encoders that have changed
-            counts += changed
-
-
-
-            # set the previous encoder value
-            prevs = vals
-
-
-        return (counts[0] + counts[1])/2./self.sample_duration, \
-               (counts[2] + counts[3])/2./self.sample_duration
-
-
-class AsyncEncoders:
-    def __init__(self):
-        self.sample_duration = 0.1
-        self.data = Array('i', np.zeros(SAMPLE_ARRAY_SIZE + 1, dtype=int))
-        self.sampler_process = Process(target=self.run, name='encoders', args=(data,))
-        self.sampler_process.start()
-        self.sampler_process.join()
-
-
-    def run(self, data):
+    def stop(self):
         for pin in p.encoder_pins:
-            GPIO.setup(pin, GPIO.IN)
-
-
-        for pin in p.encoder_pins:
-            GPIO.cleanup(GPIO.IN)
-
-    def get_speed(self):
-        start = time.time()
-        counts = np.zeros(4)
-        times = np.zeros(4)
-        corresponding = [1, 0, 3, 2]
-        prev_times = np.ones(4) * time.time()
-        prevs = np.array([GPIO.input(pin) for pin in p.encoder_pins])
-
-        while time.time() - start < self.sample_duration:
-            t = time.time()
-            # get the newest encoder value
-            vals = np.array([GPIO.input(pin) for pin in p.encoder_pins])
-
-            # see which pins have changed state
-            changed = (vals != prevs)
-
-            # # update the most recent flip times for the encoders that have chagned
-            # prev_times[changed] = t
-
-            # times[corresponding[changed]] += t - prevtimes[changed]
-
-            # # increment the counts for the encoders that have changed
-            counts += changed
-
-
-
-            # set the previous encoder value
-            prevs = vals
-
-
-        return (counts[0] + counts[1])/2./self.sample_duration, \
-               (counts[2] + counts[3])/2./self.sample_duration
-
+            GPIO.cleanup(pin)
 
 
 if __name__ == '__main__':
 
     e = Encoders()
-    while True:
-        print e.get_speed()
 
+    while True:
+        print e.speed()
+        time.sleep(0.1)
+
+    e.stop()
 
 
