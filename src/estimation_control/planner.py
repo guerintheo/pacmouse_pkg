46ef3c2ad51214a0a658890d69d2f15df4b3ab78
Peter Huson <peter@huson.com>
2019-03-23 18:04:41 -0400

    Working on macaroni Planner

--- src/estimation_control/planner.py
+++ src/estimation_control/planner.py
@@ -1,30 +1,46 @@
 import numpy as np
 import pacmouse_pkg.src.params as p
 from pacmouse_pkg.src.utils.math_utils import wrap
+from pacmouse_pkg.src.utils.maze import Maze2
+
 
 STRAIGHT = 0
 LEFT = 1
 RIGHT = 2
 
-def make_plan(state, maze, list_of_maze_indices):
+class Planner:
 
-    
+    def __init__(self, maze):
+        self.maze = maze
 
-def find_cell_type(list_of_next_cells):
-    """
-    Translates a list of cells to the type of the second cell. 
-    list_of_next_cells: An np.array of 2 or 3 next cells, in [r,c] format. 
-
-    Output: enum STRAIGHT, LEFT, RIGHT
-    """
-    if list_of_next_cells.shape[0] == 2:
-        return STRAIGHT
-    
-    elif list_of_next_cells.shape[0] == 3:
-        if (np.all(list_of_next_cells == list_of_next_cells[0,:], axis=0)[0] 
-            or np.all(list_of_next_cells == list_of_next_cells[0,:], axis=0)[1]):
-            return STRAIGHT
-
+    def make_plan(self, state, list_of_maze_indices):
+        maze = self.maze
         
 
+    def find_cell_type(self, list_of_next_cells):
+        """
+        Translates a list of cells to the type of the second cell. 
+        list_of_next_cells: An np.array of 2 or 3 next cells, in [r,c] format. 
 
+        Output: enum STRAIGHT, LEFT, RIGHT
+        """
+        if list_of_next_cells.shape[0] == 2:
+            return STRAIGHT
+        
+        elif list_of_next_cells.shape[0] == 3:
+            if (np.all(list_of_next_cells == list_of_next_cells[0,:], axis=0)[0] 
+                or np.all(list_of_next_cells == list_of_next_cells[0,:], axis=0)[1]):
+                return STRAIGHT
+            
+        return false
+
+
+if __name__ == '__main__':
+    m = Maze2(3,3)
+    
+    print(m)
+
+    p = Planner(m)
+
+    straight = p.find_cell_type(np.array([[0,1], [1,1], [2,1]]))
+    print(straight)
---
+++ src/testing/planner_unit_tests.py
@@ -0,0 +1,72 @@
+import os,sys,inspect
+import unittest
+import numpy as np
+from pacmouse_pkg.src.utils.maze import Maze
+
+class IOTest(unittest.TestCase):
+    """
+    Tests IO for Maze class. Contains basic test cases.
+
+    Each test function instantiates a Maze and checks that all returned arrays/frames are probability distributions and sum to 1.
+    """
+
+    def configure_test_maze(self):
+        m = Maze(3,3)
+        m.set_connected([0,0],[0,1],0)
+        m.set_connected([1,1],[2,1],0)
+        m.set_connected([2,2],[2,1], 0)
+        m.set_connected([1,2],[1,1], 0)
+        return m
+
+    def test_adjacency_loading(self):
+        test_adj = np.array([[0, 1, 0, 0],\
+            [1, 0, 0, 1],\
+            [0, 0, 0, 1],\
+            [0, 1, 1, 0]])
+        m = Maze(2,2,adj_matrix=test_adj)
+        prob = m.get_connected([0,0],[1,0])
+        self.assertEqual(prob, 1, 'Passing our own adj_matrix is borked')
+        prob = m.get_connected([0,0],[0,1])
+        self.assertEqual(prob, 0, 'Passing our own adj_matrix is borked')
+
+
+    def test_adjacency_basics(self):
+        m = self.configure_test_maze()
+        m.set_connected([2,0],[2,1], 0.5)
+        prob = m.get_connected([2,0],[2,1])
+        self.assertEqual(prob, 0.5, 'Get/Set adjacency matrix is borked')
+
+        self.assertRaisesRegexp(AssertionError, 'Cells must be adjacent', m.get_connected, [0,0], [2,0])
+
+    def test_maze_solving(self):
+        m = self.configure_test_maze()
+        start, end = [0,0], [2,2]
+        path1 = m.get_path(start, end)
+        print('Path found: %s from %s to %s \n%s' % (path1, start, end, m))
+
+        # This test is out of date due to a random maze being generated every time the maze is instantiated. 
+
+        # self.assertTrue(np.array_equal(path1, [0,1,4,3,6,7,8]), 'Pathfinding is borked')
+
+        start, end = [0,0], [1,0]
+        path2 = m.get_path(start, end)
+        print('Path found: %s from %s to %s \n%s' % (path2, start, end, m))
+        
+        # This test is out of date due to a random maze being generated every time the maze is instantiated. 
+
+        # self.assertTrue(np.array_equal(path2, [0,1]), 'Pathfinding to an adjacent square')
+
+        start, end = [0,0], [0,0]
+        path3 = m.get_path(start, end)
+        self.assertTrue(np.array_equal(path3, []), 'Finding path from a square to itself should return an empty array')
+
+    def test_set_connected(self):
+        # TODO: set_connected() should validate input and make sure the input coords aren't out of bounds. 
+        pass
+    
+    def test_generated_maze(self):
+        # TODO: generate_random_maze() should always produce a connected maze. 
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
