35b0f19b899dfeb0090ccb1f49a29d5e7a0a4eb0
Izzy Brand <isaiah_brand@brown.edu>
2019-04-03 00:42:34 -0400

    rewrote encoders as a ros node

--- src/hardware_interfaces/encoders.py
+++ src/hardware_interfaces/encoders.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as GPIO
 import time
+import rospy
+from geometry_msgs.msg import Vector3  # TODO: Use a better message type
 import pacmouse_pkg.src.params as p
 import numpy as np
 from multiprocessing import Process, Array
@@ -10,95 +12,102 @@ GPIO.setwarnings(False)
 
 class Encoders:
     def __init__(self):
-        self.prevs = dict()
+        rospy.init_node('encoder_node')
+        self.publisher = rospy.Publisher('/pacmouse/encoders', Vector3, queue_size=1)
+
+        self.counts = dict()
         self.most_recent_rising = dict()
-        self.left_forward = True
-        self.right_forward = True
+        self.most_recent_falling = dict()
+        self.left_direction_count = 0
+        self.right_direction_count = 0
+        self.alpha = 0.5
         
         for i, pin in enumerate(p.encoder_pins):
             GPIO.setup(pin, GPIO.IN)
             GPIO.add_event_detect(pin, GPIO.BOTH, lambda p: self._callback(p))
-            self.prevs[pin] = []
-            self.most_recent_rising[pin] = False
+            self.counts[pin] = 0
+            self.most_recent_rising[pin] = time.time()
+            self.most_recent_falling[pin] = time.time()
+
+        self.spin()
 
     def _callback(self, pin):
-        self.most_recent_rising[pin] = GPIO.input(pin)
         t = time.time()
-        self.prevs[pin].append(t)
-        # if pin == p.enc_l_b and len(self.prevs[p.enc_l_a]) > 1:
-        #     time_between_different_pins = t - self.prevs[p.enc_l_a][-1]
-        #     time_between_same_pin = self.prevs[p.enc_l_a][-1] - self.prevs[p.enc_l_a][-2]
-        #     self.left_forward = (time_between_different_pins > time_between_same_pin/2.)
-        #     # print self.left_forward
-        # elif pin == p.enc_r_b and len(self.prevs[p.enc_r_a]) > 1:
-        #     time_between_different_pins = t - self.prevs[p.enc_r_a][-1]
-        #     time_between_same_pin = self.prevs[p.enc_r_a][-1] - self.prevs[p.enc_r_a][-2]
-        #     self.right_forward = (time_between_different_pins > time_between_same_pin/2.)
-        #     # print self.right_forward
+
+        if GPIO.input(pin): # if this is a rising edge
+            if pin == p.enc_l_a:
+                t1 = self.most_recent_rising[p.enc_l_b] - self.most_recent_rising[p.enc_l_a]
+                t2 = t - self.most_recent_rising[p.enc_l_b]
+                self.left_direction_count += (t2 > t1) * 2 - 1
+            elif pin == p.enc_l_b:
+                t1 = self.most_recent_rising[p.enc_l_a] - self.most_recent_rising[p.enc_l_b]
+                t2 = t - self.most_recent_rising[p.enc_l_a]
+                self.left_direction_count += (t2 < t1) * 2 - 1
+            elif pin == p.enc_r_a:
+                t1 = self.most_recent_rising[p.enc_r_b] - self.most_recent_rising[p.enc_r_a]
+                t2 = t - self.most_recent_rising[p.enc_r_b]
+                self.right_direction_count += (t2 < t1) * 2 - 1
+            elif pin == p.enc_r_b:
+                t1 = self.most_recent_rising[p.enc_r_a] - self.most_recent_rising[p.enc_r_b]
+                t2 = t - self.most_recent_rising[p.enc_r_a]
+                self.right_direction_count += (t2 > t1) * 2 - 1
+
+            self.most_recent_rising[pin] = t
+            
+        else:
+            if pin == p.enc_l_a:
+                t1 = self.most_recent_falling[p.enc_l_b] - self.most_recent_falling[p.enc_l_a]
+                t2 = t - self.most_recent_falling[p.enc_l_b]
+                self.left_direction_count += (t2 > t1) * 2 - 1
+            elif pin == p.enc_l_b:
+                t1 = self.most_recent_falling[p.enc_l_a] - self.most_recent_falling[p.enc_l_b]
+                t2 = t - self.most_recent_falling[p.enc_l_a]
+                self.left_direction_count += (t2 < t1) * 2 - 1
+            elif pin == p.enc_r_a:
+                t1 = self.most_recent_falling[p.enc_r_b] - self.most_recent_falling[p.enc_r_a]
+                t2 = t - self.most_recent_falling[p.enc_r_b]
+                self.right_direction_count += (t2 < t1) * 2 - 1
+            elif pin == p.enc_r_b:
+                t1 = self.most_recent_falling[p.enc_r_a] - self.most_recent_falling[p.enc_r_b]
+                t2 = t - self.most_recent_falling[p.enc_r_a]
+                self.right_direction_count += (t2 > t1) * 2 - 1
+                
+            self.most_recent_falling[pin] = t
 
 
-    def speed(self, history=0.1):
-        # TODO(izzy): estimate the direction of rotation by looking at the time diffs
-        # between the a and b pins for each wheel
-        
-        t = time.time()
-        counts = dict()
+        self.counts[pin] += 1
 
-        for k in self.prevs.keys():
-            prev = np.array(self.prevs[k])
-            prev = prev[prev > t-history]
-            counts[k] = prev.size
-            self.prevs[k] = list(prev)
+    def spin(self):
+        r = rospy.Rate(p.encoder_freq)
 
-        len_l_a = len(self.prevs[p.enc_l_a])
-        len_l_b = len(self.prevs[p.enc_l_b])
+        left_count = 0
+        right_count = 0
+        encoder_msg = Vector3()
+        while not rospy.is_shutdown():
+            left_count_new = (self.counts[p.enc_l_a] + self.counts[p.enc_l_b]) / 2. * np.sign(self.left_direction_count)
+            right_count_new = (self.counts[p.enc_r_a] + self.counts[p.enc_r_b]) / 2. * np.sign(self.right_direction_count)
 
+            left_count = self.alpha * left_count_new + (1 - self.alpha) * left_count
+            right_count = self.alpha * right_count_new + (1 - self.alpha) * right_count
 
+            encoder_msg.x = left_count
+            encoder_msg.y = right_count
 
-        if len_l_a > 2 and  len_l_b > 2:
+            self.publisher.publish(encoder_msg)
 
-            l_a_start_index = -min(len_l_a,len_l_b)
-            l_b_start_index = l_a_start_index
-            l_a_end_index = -1
-            l_b_end_index = -1
+            for pin in p.encoder_pins:
+                self.counts[pin] = 0
 
-            if not self.most_recent_rising[p.enc_l_a] == self.most_recent_rising[p.enc_l_b]:
-                if self.prevs[p.enc_l_a][-1] > self.prevs[p.enc_l_b][-1]:
-                    l_a_end_index = -2
-                    l_b_start_index += 1
-                else:
-                    l_b_end_index = -2
-                    l_a_start_index += 1
+            self.left_direction_count = 0
+            self.right_direction_count = 0
 
-            diffs = np.array(self.prevs[p.enc_l_a][l_a_start_index:l_a_end_index]) -\
-                    np.array(self.prevs[p.enc_l_b][l_b_start_index:l_b_end_index])
-
-            print np.mean(diffs) > 0
-
-            # print np.max(time_between_same_pin)/np.min(time_between_same_pin) 
+            r.sleep()
 
 
-
-        # speeds = np.array(((counts[p.enc_l_a] + counts[p.enc_l_b])/(2.*history*p.gear_ratio),
-        #         (counts[p.enc_r_a] + counts[p.enc_r_b])/(2.*history*p.gear_ratio)))
-
-        # directions =  (np.array([self.left_forward, self.right_forward], dtype=int) * 2 - 1)
-        # velocities = directions * speeds
-        # return velocities
-
-    def stop(self):
-        for pin in p.encoder_pins:
-            GPIO.cleanup(pin)
 
 
 if __name__ == '__main__':
-
     e = Encoders()
 
-    while True:
-        e.speed()
-        time.sleep(0.1)
-
-    e.stop()
 
 
