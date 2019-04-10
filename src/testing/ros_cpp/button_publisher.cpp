43aed8d85146bb51569c7d49f23a67f120573700
Izzy Brand <isaiah_brand@brown.edu>
2019-04-10 00:52:24 -0400

    button publisher is slightly more concise. still broken

--- src/testing/ros_cpp/button_publisher.cpp
+++ src/testing/ros_cpp/button_publisher.cpp
@@ -6,24 +6,22 @@
 using namespace std;
 
 class Buttons {
-	int pins[4];
-	ros::Publisher publishers[4];
 public:
 	Buttons(int p[4], ros::NodeHandle n) {
 
 		char channel[20];
 		
 		for (int i = 0; i < 4; i++) {
-			int pin = pins[i] = p[i]; // copy the pin into the pins array
+			int pin = p[i];
+
+			// setup a publisher
+			sprintf(channel, "/pacmouse/buttons/%d", i+1);
+			ros::Publisher publisher = n.advertise<std_msgs::Int8>(channel, 1);
 
 			// configure the gpio
 			gpioSetMode(pin, PI_INPUT);
 			gpioSetPullUpDown(pin, PI_PUD_UP);
-			gpioSetAlertFuncEx(pin, callback, this);
-
-			// and setup a publisher
-			sprintf(channel, "/pacmouse/buttons/%d", i+1);
-			publishers[i] = n.advertise<std_msgs::Int8>(channel, 1);
+			gpioSetAlertFuncEx(pin, callback, &publisher);
 		}
 	};
 
@@ -32,16 +30,10 @@ public:
 
 
 void Buttons::callback(int gpio, int level, uint32_t tick, void *user) {
-
-   // pass the calling instance of the button class through so we can access its attributes
-   Buttons *b = (Buttons *) user;
-
-   // get the index of the gpio so we can look up the corresponding publisher
-   int i = std::distance(b->pins, std::find(b->pins, b->pins + 4, gpio));
-   printf("pin %d is %d\n", i, level);
+   ros::Publisher *publisher = (ros::Publisher *)user;
    std_msgs::Int8 msg;
    msg.data = level;
-   b->publishers[i].publish(msg);
+   publisher->publish(msg);
 }
 
 int main(int argc, char **argv)
