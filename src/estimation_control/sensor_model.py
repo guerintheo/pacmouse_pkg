fixed lidar rotation in sensor models, and removed printing from button publisher

src/estimation_control/sensor_model.py
@@ -226,6 +226,18 @@ def estimate_lidar_returns_multi(poses, maze, transparency_threshold=0.5, return
     # this is an [m] array
     lidar_local_thetas = p.lidar_transforms[:, 2]
 
+    # this is an [n x 2 x 2] array
+    pose_rotation_matricies = np.moveaxis(
+                                 np.array([[np.cos(poses[:,2]), -np.sin(poses[:,2])],
+                                           [np.sin(poses[:,2]), np.cos(poses[:,2])]]),
+                                 2, 0)
+
+    # this is an [m x 2] array
+    lidar_local_xys = p.lidar_transforms[:, :2]
+
+    # [n x m x 2] array
+    lidar_global_xys = poses[:, None, :2] + np.einsum('nij,mj->nmi', pose_rotation_matricies, lidar_local_xys)
+
     # this is an [n x m] array
     lidar_global_thetas = poses[:, 2, None] + lidar_local_thetas[None, :]
 
@@ -235,11 +247,6 @@ def estimate_lidar_returns_multi(poses, maze, transparency_threshold=0.5, return
                                                [np.sin(lidar_global_thetas), np.cos(lidar_global_thetas)]]),
                                      [2,3], [0,1])
 
-    # this is an [m x 2] array
-    lidar_local_xys = p.lidar_transforms[:, :2]
-
-    # [n x m x 2] array
-    lidar_global_xys = poses[:, None, :2] + np.einsum('nmij,mj->nmi', lidar_global_rotation_matrices, lidar_local_xys)
 
     # [n x m x 2] array
     lidar_global_vecs = np.einsum('nmij,j->nmi', lidar_global_rotation_matrices, np.array([1,0]))
@@ -403,7 +410,7 @@ def update_walls(pose, lidars, maze, decrement_amount=0.05, increment_amount=0.0
     lidar_local_xys = p.lidar_transforms[:, :2]
 
     # [m x 2] array
-    lidar_global_xys = pose[None, :2] + np.einsum('mij,mj->mi', lidar_global_rotation_matrices, lidar_local_xys)
+    lidar_global_xys = pose[None, :2] + np.einsum('ij,mj->mi', rotation_matrix_2d(pose[2]), lidar_local_xys)
 
     # [m x 2] array
     lidar_global_vecs = np.einsum('mij,mj->mi', lidar_global_rotation_matrices, np.array([[1,0]]) * lidars[:, None])
@@ -584,7 +591,7 @@ def test_estimate_lidar_returns_multi():
     plt.ylim(-border, maze.height * p.maze_cell_size+border)
 
     # generate poses
-    n = 50
+    n = 3
     poses = np.array([np.random.rand(n)*maze.width * p.maze_cell_size,
                       np.random.rand(n)*maze.height * p.maze_cell_size,
                       np.random.rand(n)*np.pi*2]).T

src/simulator.py
@@ -79,7 +79,7 @@ class Simulator:
         self.real_bot_state += np.random.normal(u_mu, self.u_sigma)
 
         # get the sensor data (with noise)
-        lidars = estimate_lidar_returns_multi(self.real_bot_state[None,:3], self.maze) + np.random.normal(0, self.lidar_sigma, 6)
+        lidars = estimate_lidar_returns_multi(self.real_bot_state[None,:3], self.maze) + np.random.normal(0, self.lidar_sigma, p.num_lidars)
         encoders = cmd + np.random.normal(0, self.encoder_sigma, size=2)
 
         return lidars, encoders
@@ -185,7 +185,7 @@ class DrivingSimulator:
         self.real_bot_state += np.random.normal(u_mu, self.u_sigma)
 
         # get the sensor data (with noise)
-        lidars = estimate_lidar_returns_multi(self.real_bot_state[None,:3], self.real_maze)[0] + np.random.normal(0, self.lidar_sigma, 6)
+        lidars = estimate_lidar_returns_multi(self.real_bot_state[None,:3], self.real_maze)[0] + np.random.normal(0, self.lidar_sigma, p.num_lidars)
         encoders = cmd + np.random.normal(0, self.encoder_sigma, size=2)
 
         return lidars, encoders
@@ -306,7 +306,7 @@ class FullSimulator:
         self.real_bot_state += np.random.normal(u_mu, self.u_sigma)
 
         # get the sensor data (with noise)
-        lidars = estimate_lidar_returns_multi(self.real_bot_state[None,:3], self.real_maze)[0] + np.random.normal(0, self.lidar_sigma, 6)
+        lidars = estimate_lidar_returns_multi(self.real_bot_state[None,:3], self.real_maze)[0] + np.random.normal(0, self.lidar_sigma, p.num_lidars)
         encoders = cmd + np.random.normal(0, self.encoder_sigma, size=2)
 
         return lidars, encoders